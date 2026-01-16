84b4bbbd67e23fef1f9494dfa29b4814a334f171
eder-castro <eder.castro@live.com>
2026-01-16 19:38:48 -0300

    Criei um lembrete de ativação do .venv no windows e comecei a resolucao do desafio na empresa.

--- desafio_final.py
+++ desafio_final.py
@@ -6,3 +6,34 @@
     #Conta a quantidade de avaliacoes positivas
     # Une cada item da lista em uma variavel tipo string com algum separador
 # Ao final retorna a quantidade e a variavel do passo 4 
+from openai import OpenAI
+import os
+import pandas as pd
+
+### Importando o arquivo .txt para dentro de um dataframe pandas
+df_resenhas = pd.read_csv('resenhas.txt', sep='$', names=['ID', 'USER', "RESENHA"], encoding="utf-8")
+#print(df_resenhas)
+coluna_resenhas = df_resenhas["RESENHA"]
+#print(coluna_resenhas)
+
+nova_coluna = []
+
+client_openai = OpenAI(
+    base_url="http://127.0.0.1:1234/v1",
+    api_key="lm-studio"
+)
+
+def obter_classificacoes(resenhas):
+    for linha in resenhas:
+        retorno_llm = client_openai.chat.completions.create(
+            model="google/gemma-3-4b",
+            messages=[{"role": "user",
+                "content":f"Vou te enviar uma lista de resenhas sobre produtos ou serviços e preciso que você classifique o texto em 3 categorias: Positivo, Neutro ou Negativo. Na dúvida, use 'NEUTRO'. Não quero justificativa, não quero nenhum outro texto, o retorno deve ser somente uma das 3 palavras. Segue lista: {linha}"
+            }],
+        )
+        nova_coluna.append(retorno_llm.choices[0].message.content.strip())
+
+obter_classificacoes(coluna_resenhas)
+#print(nova_coluna)
+df_resenhas["AVALIACAO"] = nova_coluna
+print(df_resenhas)
