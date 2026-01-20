e521eadf8221d4ba13af144f37e7d68a16574597
eder-castro <eder.castro@live.com>
2026-01-20 14:30:44 -0300

    Alteração do classificador de livros para uso de várias API-KEY para evitar sobrecarga de requisicoes e estouro da cota diária Free

--- classifica_livros.py
+++ classifica_livros.py
@@ -7,32 +7,55 @@ import pandas as pd
 load_dotenv()
 GOOGLE_API_KEY_01 = os.getenv("GEMINI_API_KEY_01")
 GOOGLE_API_KEY_02 = os.getenv("GEMINI_API_KEY_02")
+GOOGLE_API_KEY_03 = os.getenv("GEMINI_API_KEY_02")
+GOOGLE_API_KEY_04 = os.getenv("GEMINI_API_KEY_04")
 
+cliente1 = genai.Client(api_key=GOOGLE_API_KEY_01)
+cliente2 = genai.Client(api_key=GOOGLE_API_KEY_02)
+cliente3 = genai.Client(api_key=GOOGLE_API_KEY_03)
+cliente4 = genai.Client(api_key=GOOGLE_API_KEY_04)
+
+contador = 0
+##### CRIA LISTA DE LIVROS #####
 lista_livros = []
+
+##### ABRE O ARQUIVO TXT E CARREGA NA LISTA DE LIVROS #####
 with open("livros.txt", "r", encoding="utf-8") as arquivo:
     for linha in arquivo:
         lista_livros.append(linha.strip())
 
+##### CRIA LISTA DE CLASSIFICACOES #####
 classificacoes = []
+
+##### CRIA FUNCAO PARA OBTER CATEGORIA DOS LIVROS #####
 def obter_categorias(nome_do_livro):
-    cliente = genai.Client(api_key=GOOGLE_API_KEY_02)
+    ##### Se contador chegou a 19, usa outra chave de API
+    global contador
+    if contador <= 19:
+        cliente = cliente3
+    else:
+        cliente = cliente4
+    ##### Chama o LLM passando o nome do livro e armazena Categoria devolvida na variavel "resposta"
     resposta = cliente.models.generate_content(
         model = "gemini-flash-lite-latest",
         contents = f'''Vou te enviar o nome de um livro e quero que você obtenha a categoria deste livro em uma palavra. Você deve retornar somente a categoria, sem explicações nem textos adicionais.
-        
         Exemplo:
-        Livro: A arte da guerra
-        Resenha: AutoAjuda
-
+        "Livro: A arte da guerra
+        Resenha: AutoAjuda"
         Segue nome do livro: {nome_do_livro}'''
     )
-    #print("Esperando 60 segundos para não bloquear...\n")
-    #time.sleep(30)
+    ##### Atualiza o último dicionário existente dentro da lista de dicionários com a chave-valor da categoria
     classificacoes[-1].update({f"Categoria:":resposta.text})
 
+##### CRIA FUNCAO PARA OBTER RESENHA DOS LIVROS #####
 def obter_resenhas(lista_de_livros):
-    cliente = genai.Client(api_key=GOOGLE_API_KEY_01)
-    conta = 0
+    ##### Se contador chegou a 19, usa outra chave de API
+    global contador
+    if contador <= 19:
+        cliente = cliente1
+    else:
+        cliente = cliente2
+    ##### Chama o LLM passando o nome do livro e armazena Resenha devolvida na variavel "resposta"
     for livro in lista_de_livros:
         resposta = cliente.models.generate_content(
             model = "gemini-flash-lite-latest",
@@ -44,21 +67,30 @@ def obter_resenhas(lista_de_livros):
 
             Segue nome do livro: {livro}'''
         )
-        #print("Esperando 30 segundos para não bloquear...\n")
+        ##### Exibe uma contagem regressiva na tela, aguardando para enviar requisicao ao LLM depois deste tempo, evitando estouro de cota
         for cont in range(30, -1, -1):
             print(f"{cont:02d}", end='\r', flush=True)
             time.sleep (1)
-        #time.sleep(30)
-        classificacoes.append({f"Livro:":livro ,"Resenha:":resposta.text})
-        obter_categorias(livro)
-        conta+=1
-        print(f"Livro {conta} OK!")
-        #print("OK")
-print(classificacoes)
 
+        ##### Insere um dicionário dentro da lista de dicionários com a chave-valor do nome do livro e a chave-valor da resenha
+        classificacoes.append({f"Livro:":livro ,"Resenha:":resposta.text})
+        ##### CHAMA A FUNCAO "OBTER CATEGORIAS"
+        obter_categorias(livro)
+        ##### INCREMENTA CONTADOR E IMPRIME NUMERO DO LIVRO PROCESSADO
+        contador+=1
+        print(f"Livro {contador} OK!")
+
+##### CHAMA A FUNCAO "OBTER RESENHAS"
 obter_resenhas(lista_livros)
+
+##### TRANSFORMA A LISTA DE DICIONARIOS EM UM DATAFRAME
+df_resenhas = pd.DataFrame(classificacoes)
+
+##### EXPORTA O DATAFRAME PARA UM ARQUIVO CSV
+df_resenhas.to_csv("livros_classificados.csv",index="false", encoding="utf-8")
+
+##### APOS FINALIZAR O PROCESSO, MOSTRA UM TEXTO INDICANDO QUE OS DADOS ESTAO NO ARQUIVO CRIADO
 print('Resenha e Classificação obtidas. Veja o resultado no arquivo "livros_classificados.csv"')
 
+##### IMPRIME A LISTA DE DICIONARIOS COM NOME DOS LIVROS, RESENHAS E CLASSIFICACOES
 #print(classificacoes)
-df_resenhas = pd.DataFrame(classificacoes)
-df_resenhas.to_csv("livros_classificados.csv",index="false", encoding="utf-8")
